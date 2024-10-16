25250a81f0cc46a7e62e323b834451567bb303b8
Ovilia <zwl.sjtu@gmail.com>
2024-10-16 18:07:26 +0800

    feat(lineRange): complete lineRange

--- custom-series/lineRange/src/index.ts
+++ custom-series/lineRange/src/index.ts
@@ -17,13 +17,38 @@
  * under the License.
  */
 
-import echarts from 'echarts';
+import echarts, { zrUtil } from 'echarts';
 import type {
   CustomElementOption,
   CustomSeriesRenderItem,
 } from 'echarts/types/src/chart/custom/CustomSeries.d.ts';
 import type { EChartsExtensionInstallRegisters } from 'echarts/src/extension.ts';
 import type { Polygon, Polyline } from 'echarts/src/util/graphic.ts';
+
+type LineRangeItemPayload = {
+  lineStyle?: {
+    color?: string;
+    opacity?: number;
+    width?: number;
+    type?: string;
+    dashOffset?: number;
+    cap?: 'butt' | 'round' | 'square';
+    join?: 'bevel' | 'round' | 'miter';
+    miterLimit?: number;
+    shadowBlur?: number;
+    shadowColor?: string;
+    shadowOffsetX?: number;
+    shadowOffsetY?: number;
+  };
+  areaStyle?: {
+    color?: string;
+    opacity?: number;
+    shadowBlur?: number;
+    shadowColor?: string;
+    shadowOffsetX?: number;
+    shadowOffsetY?: number;
+  };
+};
 
 const renderItem = (
   params: echarts.CustomSeriesRenderItemParams,
@@ -37,27 +62,73 @@ const renderItem = (
   // If is the last item, render the line range
   const cnt = params.dataInsideLength;
   if (params.dataIndex === cnt - 1) {
+    const itemPayload = params.itemPayload as LineRangeItemPayload;
+
     const points: number[][] = [];
+    let pathDataStart: string = '';
+    let pathDataEnd: string = '';
     for (let i = 0; i < cnt; i++) {
       const startValue = api.value(1, i);
       const startCoord = api.coord([i, startValue]);
       points.push(startCoord);
+      pathDataStart +=
+        (i === 0 ? 'M' : 'L') + startCoord[0] + ',' + startCoord[1] + ' ';
     }
     for (let i = cnt - 1; i >= 0; i--) {
       const endValue = api.value(2, i);
       const endCoord = api.coord([i, endValue]);
       points.push(endCoord);
+      pathDataEnd +=
+        (i === cnt - 1 ? 'M' : 'L') + endCoord[0] + ',' + endCoord[1] + ' ';
     }
-    const polygon = {
-      type: 'polygon',
+
+    if (itemPayload.areaStyle) {
+      const areaStyle = itemPayload.areaStyle;
+      group.children.push({
+        type: 'polygon',
+        shape: {
+          points,
+        },
+        style: {
+          fill: areaStyle.color || api.visual('color'),
+          opacity: zrUtil.retrieve2(areaStyle.opacity, 0.2),
+        },
+        silent: true,
+      } as Polygon);
+    }
+
+    const lineStyle = itemPayload.lineStyle || {};
+    const polylineStyle = {
+      fill: 'none',
+      stroke: lineStyle.color || api.visual('color'),
+      lineWidth: zrUtil.retrieve2(lineStyle.width, 0),
+      opacity: zrUtil.retrieve2(lineStyle.opacity, 1),
+      type: lineStyle.type,
+      dashOffset: lineStyle.dashOffset,
+      lineCap: lineStyle.cap,
+      lineJoin: lineStyle.join,
+      miterLimit: lineStyle.miterLimit,
+      shadowBlur: lineStyle.shadowBlur,
+      shadowColor: lineStyle.shadowColor,
+      shadowOffsetX: lineStyle.shadowOffsetX,
+      shadowOffsetY: lineStyle.shadowOffsetY,
+    };
+    group.children.push({
+      type: 'path',
       shape: {
-        points,
+        pathData: pathDataStart,
       },
-      style: {
-        fill: api.visual('color'),
+      style: polylineStyle,
+      silent: true,
+    } as Polyline);
+    group.children.push({
+      type: 'path',
+      shape: {
+        pathData: pathDataEnd,
       },
-    } as Polygon;
-    group.children.push(polygon);
+      style: polylineStyle,
+      silent: true,
+    } as Polyline);
   }
   return group;
 };
